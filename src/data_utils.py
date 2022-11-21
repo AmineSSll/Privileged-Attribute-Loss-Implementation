f690c7dd703603b0524d996bed1708fdd387065e
yozaii <73235941+yozaii@users.noreply.github.com>
2022-11-22 01:16:29 +0200

    Still setting up overview of the project

---
+++ src/data_utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+# ======================================================= #
+# ================ DATA WRITING FUNCTIONS =============== #
+# ======================================================= #
+
+
+# https://www.tensorflow.org/tutorials/load_data/images
+
+
+def download_dataset_to_dir():
+    pass
+
+def save_model_to_dir():
+    pass
+    
+# ======================================================= #
+# ================ DATA LOADING FUNCTIONS =============== #
+# ======================================================= #
+
+def load_img():
+    pass
+
+def load_imgs():
+    pass
+
+def load_label():
+    pass
+
+def load_labels():
+    pass
+
+def load_data():
+    pass
+    # return (train_imgs, train_labels), (test_imgs, test_labels)
+
+# ======================================================= #
+# =============== DATA PROCESSING FUNCTIONS ============= #
+# ======================================================= #
+
+def img_resize(img, width, height):
+    pass
+
+def create_augmented_img():
+    pass
+
+def process_imgs(imgs):
+    # img_resize(img, width, height)
+    pass
+
+def process_prior_heatmaps(imgs):
+    pass
+
+
+
+    
---
+++ src/model.py
@@ -0,0 +1,18 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+
+def create_prior_heatmap(sigma):
+    pass
+
+class PAL(layers.Layer):
+    def __init__(self):
+        pass
+        
+    def call(self):
+        pass
+    
+class PAL_CNN():
+    pass
